694ed494ed9aeb2ee10c7b13a3f4132529504d4b
ritasumtsova <48488494+ritasumtsova@users.noreply.github.com>
2022-02-04 12:00:35 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import Greet from './components/Greet';
+import Welcome from './components/Welcome';
+import Hello from './components/Hello';
+import Message from './components/Message';
+import Counter from './components/Counter';
+import Click from './components/Click';
+import ClassClick from './components/ClassClick';
+import ParentComponent from './components/ParentComponent';
+import UserGreet from './components/UserGreet';
+import List from './components/List';
+import Form from './components/Form';
+import LCMounting from './components/LCMounting';
+import LCUpdate from './components/LCUpdate';
+import { Fragment } from 'react';
+import Fragments from './components/Fragments';
+import Table from './components/Table';
+import CustomPureComponent from './components/PureComponent';
+import Refs from './components/Refs';
+import Focus from './components/Focus';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <ParentComponent /> */}
+      {/* <Click />
+      <ClassClick /> */}
+      {/* <Greet name="Rita" heroName="Wonder Woman"/>
+      <Welcome name="Rita" heroName="Wonder Woman"/> */}
+      {/* <Hello />
+      <Message />
+      <Counter /> */}
+      {/* <UserGreet /> */}
+      {/* <List /> */}
+      {/* <Form /> */}
+      {/* <LCMounting /> */}
+      {/* <LCUpdate /> */}
+      {/* <Fragments /> */}
+      {/* <Table /> */}
+      {/* <ParentComponent /> */}
+      {/* <Refs /> */}
+      <Focus />
     </div>
   );
 }
